d9f40bbcb561937ba1e44e4183cba9b0cfe69c81
Tyron Jung <tyronjung@hotmail.com>
2018-04-06 18:46:39 -0400

    Orientation bias

--- main.m
+++ main.m
@@ -5,43 +5,53 @@ cave_y = 1;
 num_ants = 10;
 grid_size = 100;
 max_iter = 1000;
+delay = 0.01;
 
 x = ones(num_ants, 1); % x coordinate of ant i.
 y = ones(num_ants, 1); % y coordinate of ant i.
 z = zeros(grid_size); % Location state matrix.
+theta = rand(num_ants, 1) * 2 * pi; % Orientation of ant i.
 has_food = zeros(num_ants, 1); % Does ant i have food?
 p_search = zeros(grid_size); % Search pheromone matrix.
 p_return = zeros(grid_size); % Return pheromone matrix.
+
+function x_new = bounded(x_old, lo, hi)
+    if x_old < lo
+        x_new = lo;
+    elseif x_old > hi
+        x_new = hi;
+    else
+        x_new = x_old;
+    end
+end
+
+function theta_new = wrapped(theta_old)
+    if theta_old < 0
+        theta_new = 2 * pi - theta_old;
+    elseif theta_old > 2 * pi
+        theta_new = theta_old - 2 * pi;
+    else
+        theta_new = theta_old;
+    end
+end
 
 figure;
 im = image(z);
 for iter = 1:max_iter
     for i = 1:num_ants
-        x_i = x(i);
-        y_i = y(i);
-        while x_i == x(i) && y_i == y(i)
-            if x_i == 1
-                x_i += [0, 1](randi(2));
-            elseif x_i == grid_size
-                x_i += [-1, 0](randi(2));
-            else
-                x_i += [-1, 0, 1](randi(3));
-            end % if
-            if y_i == 1
-                y_i += [0, 1](randi(2));
-            elseif y_i == grid_size
-                y_i += [-1, 0](randi(2));
-            else
-                y_i += [-1, 0, 1](randi(3));
-            end % if
-        end % while
-        % Move ant.
+        % Move ant out of current location.
         z(x(i), y(i)) -= 100;
-        z(x_i, y_i) += 100;
-        % Update ant position.
-        x(i) = x_i;
-        y(i) = y_i;
+        % Compute new location.
+        x(i) += round(cos(theta(i)));
+        y(i) += round(sin(theta(i)));
+        theta(i) += rand() * pi / 4 - pi / 8;
+        % Normalize.
+        x(i) = bounded(x(i), 1, grid_size);
+        y(i) = bounded(y(i), 1, grid_size);
+        theta(i) = wrapped(theta(i));
+        % Move ant into new location.
+        z(x(i), y(i)) += 100;
     end % for
-    pause(0.01);
+    pause(delay);
     set(im, 'CData', z); % Update image.
 end % for
